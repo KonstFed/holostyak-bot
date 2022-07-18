0640a1c99a794a689873950715546cc62c831a85
KonstFed <konstantin_fedorov_03@mail.ru>
2022-07-18 23:32:52 +0800

    fix path in create_table.py

--- create_table.py
+++ create_table.py
@@ -1,6 +1,10 @@
 import db_utils
 import json
+import os
 if __name__=="__main__":
+    abs_path = os.path.abspath(__file__)
+    dname = os.path.dirname(abs_path)
+    os.chdir(dname)
     f = open("configs/config.json")
     config = json.load(f)
     db = db_utils.db_manager(config["db"])
