0b64104ef3ddb52fe25d5811fd7f99f6597fa985
KonstFed <konstantin_fedorov_03@mail.ru>
2022-09-02 16:49:42 +0300

    updated DB and db_utils

--- database/db_utils.py
+++ database/db_utils.py
@@ -8,19 +8,17 @@ class db_manager:
         self.cursor.close()
         self.conn.close()
     
-    def add_row(self, chat_id, idea_name = "", description = "",season = 0, timescooked = 0):
-        self.cursor.execute("INSERT INTO ideas (chat_id, name, description, season, timescooked) VALUES(%s,%s,%s,%s,%s)",(chat_id,idea_name, description, season, timescooked))
+    def add_row(self, idea_name = "", description = "",season = 0, timescooked = 0):
+        self.cursor.execute("INSERT INTO ideas (name, description, season, timescooked) VALUES(%s,%s,%s,%s,%s)",(idea_name, description, season, timescooked))
         self.conn.commit()
-    
-    def alter_row(self,chat_id, idea_name, description, season = 0, timescooked = 0):
-        pass
-    def get_all(self, chat_id):
-        self.cursor.execute("""SELECT * FROM ideas WHERE chat_id = %s;""",(str(chat_id),))
+
+    def get_all(self):
+        self.cursor.execute("""SELECT * FROM ideas""")
         records = self.cursor.fetchall()
         return records
 
     def create_table(self, table_name):
         command = "CREATE TABLE " + table_name + \
-        " (id SERIAL PRIMARY KEY, chat_id text, name text, description text, season integer, timescooked integer)"
+        " (id SERIAL PRIMARY KEY, name text, description text, season integer, timescooked integer)"
         self.cursor.execute(command)
         self.conn.commit()
